1c7abd749daf49e20ff930489ac2610ef4462268
Kevin SAINT PAUL <kevin.saintpaul@diiage.org>
2018-04-28 13:39:53 +0200

    Create View for Games

---
+++ LesSimpleEfficacesFront/LesSimpleEfficacesFront/LesSimpleEfficacesFront/Views/ValidGameView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LesSimpleEfficacesFront.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LesSimpleEfficacesFront.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ValidGameView : ContentPage
+	{
+		public ValidGameView ()
+		{
+			InitializeComponent ();
+
+		    BindingContext = new ValidGameViewModel();
+		}
+	}
+}
